88f19ed1235db005332f3795e22e9d09afc28857
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-12-19 12:55:43 +0800

    fix bug

--- hosts/paridis/default.nix
+++ hosts/paridis/default.nix
@@ -1,9 +1,12 @@
 { config, pkgs, lib, ... }:
-let
-	dir = ./.;
-	files = builtins.filter (f: f != "default.nix" && builtins.match ".+\\.nix$" f != null) (builtins.attrNames (builtins.readDir dir));
-	modules = map (f: ./${f}) files;
-in {
-	imports = modules;
+
+{
+	imports = [
+    ./hardware-configuration.nix
+    ./configuration.nix
+    ./nvidia.nix
+    ./chinese.nix
+    ./programs.nix
+  ];
 }
 
