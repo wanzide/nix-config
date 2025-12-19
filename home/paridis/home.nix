786bc74966512a62a35f6ec0db8d2b17bc661807
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-12-19 13:31:34 +0800

    remove dms

--- home/paridis/home.nix
+++ home/paridis/home.nix
@@ -6,34 +6,9 @@
   home.homeDirectory = "/home/paridis";
   home.stateVersion = "25.11";
 
-  imports = [
-    inputs.dms.homeModules.dankMaterialShell.default
-    inputs.dms.homeModules.dankMaterialShell.niri
-  ];
-
   programs.zsh.enable = true;
 
   programs.niri.enable = true;
-
-  programs.dankMaterialShell = {
-    enable = true;
-    niri = {
-      enableKeybinds = true;   # Sets static preset keybinds
-      enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
-    };
-    systemd = {
-      enable = true;             # Systemd service for auto-start
-      restartIfChanged = true;   # Auto-restart dms.service when dankMaterialShell changes
-    };
-    
-    # Core features
-    enableSystemMonitoring = true;     # System monitoring widgets (dgop)
-    enableClipboard = true;            # Clipboard history manager
-    enableVPN = true;                  # VPN management widget
-    enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
-    enableAudioWavelength = true;      # Audio visualizer (cava)
-    enableCalendarEvents = true;       # Calendar integration (khal)
-  };
 
   home.packages = with pkgs; [
     google-chrome
