88cc234bf9d8704864ffb306806bf07794db5a03
rhizoma_paridis <rhizoma_paridis@outlook.com>
2025-12-19 12:52:12 +0800

    fix bug

--- hosts/paridis/default.nix
+++ hosts/paridis/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 let
 	dir = ./.;
-	files = builtins.filter (f: f != "default.nix" && builtins.match ".+\\.nix$" f != null) (builtins.readDir dir);
+	files = builtins.filter (f: f != "default.nix" && builtins.match ".+\\.nix$" f != null) (builtins.attrNames (builtins.readDir dir));
 	modules = map (f: ./${f}) files;
 in {
 	imports = modules;
