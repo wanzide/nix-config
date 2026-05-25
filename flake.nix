d263888ece7a1edbc80c2737543afa7be6fdb856
rhizoma_paridis <rhizoma_paridis@outlook.com>
2026-05-25 14:37:25 +0800

    1

--- flake.nix
+++ flake.nix
@@ -1,6 +1,16 @@
 {
   description = "paridis NixOS system configuration";
 
+  # Substituters here take effect during evaluation of this flake itself
+  # (i.e. on the very first `nixos-rebuild --flake` before the system config
+  # is activated), so bootstrapping a fresh machine already uses the mirror.
+  nixConfig = {
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
